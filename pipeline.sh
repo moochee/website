a5b46a731439c99bc5d0e5b717b9bedd20f0a851
Yang Zhao <yang.zhao@sap.com>
2022-12-27 18:57:21 -0800

    timeout is not needed

--- pipeline.sh
+++ pipeline.sh
@@ -9,7 +9,7 @@ fi
 dokku apps:create www
 dokku certs:add www /home/dokku/server.crt /home/dokku/server.key
 dokku proxy:ports-set www http:80:8080 https:443:8080
-dokku nginx:set www proxy-read-timeout 180m
+# dokku nginx:set www proxy-read-timeout 180m
 
 git remote add dokku dokku@moochee.us:www || git remote set-url dokku dokku@moochee.us:www
 git push dokku main
