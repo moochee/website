0fe9fea67ae2b7579c4e1b7c0863fa94597b7074
Yang Zhao <yang.zhao@sap.com>
2023-01-01 21:30:25 -0800

    start over

--- server/main.js
+++
@@ -1,5 +0,0 @@
-import express from 'express'
-const app = express()
-app.use('/', express.static('web'))
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Website started at ${port}`))
